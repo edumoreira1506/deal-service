4d435193eeb440068a144a1aeace5e1ed1092bfe
edumoreira1506 <contato@eduardoem.com.br>
2022-09-10 18:41:36 -0300

    Add api key

--- src/constants/api-keys.ts
+++ src/constants/api-keys.ts
@@ -1,4 +1,5 @@
 export const API_KEYS_SERVICE_URL = String(process.env.API_KEYS_SERVICE_URL)
+export const ADVERTISING_SERVICE_API_KEY = String(process.env.ADVERTISING_SERVICE_API_KEY)
 
 export const API_KEYS_USER = String(process.env.API_KEYS_USER)
 export const API_KEYS_PASSWORD = String(process.env.API_KEYS_PASSWORD)
