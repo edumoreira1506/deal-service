1f4345bf36f14ca6813531edfe9bfecbfc0fbb35
edumoreira1506 <contato@eduardoem.com.br>
2022-09-10 19:00:25 -0300

    Fix middleware

--- src/middlewares/withDealParam.ts
+++ src/middlewares/withDealParam.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Response } from 'express'
 import {  BaseController, NotFoundError, withRequestParam } from '@cig-platform/core'
 
-import DealController from '@Controllers/DealController'
 import Deal from '@Entities/DealEntity'
 import { RequestWithDeal } from '@Types/requests'
 import CancelledDealError from '@Errors/CancelledDealError'
 import FinishedDealError from '@Errors/FinishedDealError'
+import DealRepository from '@Repositories/DealRepository'
 
 export const withNotFinishedDealParam =
     (req: RequestWithDeal, res: Response, next: NextFunction) =>
-      withRequestParam<Deal>('dealId', 'deal', DealController, BaseController.errorResponse)(req, res, () => {
+      withRequestParam<Deal>('dealId', 'deal', DealRepository, BaseController.errorResponse)(req, res, () => {
         try {
           if (!req.deal) throw new NotFoundError()
           if (req.deal.cancelled) throw new CancelledDealError()
@@ -21,4 +21,4 @@ export const withNotFinishedDealParam =
         }
       })
 
-export default withRequestParam<Deal>('dealId', 'deal', DealController, BaseController.errorResponse)
+export default withRequestParam<Deal>('dealId', 'deal', DealRepository, BaseController.errorResponse)
